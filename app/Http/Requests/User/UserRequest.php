975a355f9407ce5794bfb9878e97f5a81a842fa8
luanphamremvn <luanpham.remvn039@gmail.com>
2025-06-11 20:42:22 +0700

    #VDT-12 fix validate create user

--- app/Http/Requests/User/UserRequest.php
+++ app/Http/Requests/User/UserRequest.php
@@ -24,9 +24,9 @@ class UserRequest extends FormRequest
     {
         return [
             'name' =>'string|required|min:1|max:255',
-            'username' => 'string|required|unique:users|min:1|max:255',
+            'username' => 'string|required|unique:users|min:5|max:255|regex:/^\S*$/|max:255',
             'email' => 'unique:users|email|min:6|max:255',
-            'password' => 'min:8|required_with:password_confirmation|same:password_confirmation',
+            'password' => 'min:8|regex:/[^\w]/|required_with:password_confirmation|same:password_confirmation',
             'password_confirmation' => 'min:8'
         ];
     }
@@ -42,7 +42,8 @@ class UserRequest extends FormRequest
             'name.min' => 'tên sách ít nhất 1 ký tự',
             'name.max' => 'tên sách tối đa 255 ký tự',
             'username.unique' => 'username đã tồn tại',
-            'username.min' => 'username ít nhất 1 ký tự',
+            'username.regex' => 'username không được chứa khoảng trắng',
+            'username.min' => 'username ít nhất 5 ký tự',
             'username.max' => 'username tối đa 255 ký tự',
             'email.max' => 'tên tác giả tối đa 255 ký tự',
             'email.min' => 'mô tả ít nhất 6 ký tự',
@@ -53,6 +54,7 @@ class UserRequest extends FormRequest
             'password_confirmation.min' => 'mật khẩu xác nhận ít nhất 8 ký tự',
             'password_confirmation.required_with' => 'vui lòng nhập mật khẩu xác nhận',
             'password_confirmation.same' => 'mật khẩu xác nhận không khớp',
+            'password.regex' => 'mật khẩu phải chứa ít nhất một ký tự đặc biệt',
             'password_confirmation.min' => 'mật khẩu xác nhận ít nhất 8 ký tự',
             'password_confirmation.same' => 'mật khẩu xác nhận không khớp',
             'password_confirmation.required_with' => 'vui lòng nhập mật khẩu xác nhận',
--- resources/views/pages/user/create.blade.php
+++ resources/views/pages/user/create.blade.php
@@ -6,7 +6,7 @@
         class="max-w-xl mx-auto bg-white p-6">
         @csrf
         <p class="mb-10 text-3xl">Thêm tài khoản</p>
-        {{--user--}}
+        {{-- user --}}
         <div class="relative z-0 w-full mb-5 group">
             <input type="text" name="name" id="name" value="{{ old('name') }}"
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -16,21 +16,6 @@
                 Họ và tên
             </label>
             @error('name')
-                <p class="mt-2 text-sm text-red-600 dark:text-red-500">
-                    {{ $message }}
-                </p>
-            @enderror
-        </div>
-        {{--username--}}
-        <div class="relative z-0 w-full mb-5 group">
-            <input type="text" name="username" id="username" value="{{ old('username') }}"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                required />
-            <label for="username"
-                class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                Tên đăng nhập
-            </label>
-            @error('username')
                 <p class="mt-2 text-sm text-red-600 dark:text-red-500">
                     {{ $message }}
                 </p>
@@ -46,6 +31,22 @@
                 Email
             </label>
             @error('email')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                    {{ $message }}
+                </p>
+            @enderror
+        </div>
+
+        {{-- username --}}
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="text" name="username" id="username" value="{{ old('username') }}"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                required />
+            <label for="username"
+                class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                Tên đăng nhập
+            </label>
+            @error('username')
                 <p class="mt-2 text-sm text-red-600 dark:text-red-500">
                     {{ $message }}
                 </p>
@@ -85,11 +86,7 @@
         <div class="flex justify-end gap-3">
             <button type="submit"
                 class="mb-3 sm:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                Tạo User
-            </button>
-            <button type="reset"
-                class=" text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                Xoá
+                Đăng ký
             </button>
         </div>
     </form>
